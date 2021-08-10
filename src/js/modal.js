7c34ec8dc440ef4a0038f47c08a04e7e4cfed7f5
Natalia Sorokina <pticasonabird@gmail.com>
2021-08-10 13:29:14 +0300

    add functions closing modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,82 @@
-(() => {
-  const refs = {
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+// (() => {
+//   const refs = {
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
 
-  refs.closeModalBtn.addEventListener('click', closeModal);
+//   refs.closeModalBtn.addEventListener('click', closeModal);
 
-  function closeModal() {
-    refs.modal.classList.add('visually-hidden');
-  }
-})();
+//   function closeModal() {
+//     refs.modal.classList.add('visually-hidden');
+//   }
+// })();
+
+
+
+const closeModalBtn = document.querySelector('[data-modal-close]');
+const modal = document.querySelector('[data-modal]');
+const backdrop = document.querySelector('.backdrop');
+
+window.addEventListener('keydown', escKeyPressHandler);
+closeModalBtn.addEventListener('click', modalCloseHandler);
+backdrop.addEventListener('click', backdropClickHandler);
+
+
+
+function modalCloseHandler() {
+    modal.classList.add('visually-hidden');
+}
+  
+ function backdropClickHandler(event) {
+   if (event.currentTarget === event.target) {
+       backdrop.classList.add('visually-hidden');
+    }
+}
+
+ function escKeyPressHandler(event) {
+    if (event.code === 'Escape') {
+        backdrop.classList.add('visually-hidden');
+    }
+}
+
+
+
+
+// function modalWindowOpenHandler(event) {
+//     event.preventDefault();
+//     refs.btn.addEventListener('click', modalWindowCloseHandler);
+//     refs.overlay.addEventListener('click', overlayClickHandler);
+//     window.addEventListener('keydown', escKeyPressHandler);
+//     window.addEventListener('keydown', imagesScrollHandler);
+
+//     if (event.target.nodeName !== 'IMG') {
+//         return;
+//     }
+
+//     refs.lightbox.classList.add('is-open');
+//     refs.image.src = event.target.dataset.source;
+//     refs.image.alt = event.target.alt;
+// }
+
+// function modalWindowCloseHandler() {
+//     refs.btn.addEventListener('click', modalWindowCloseHandler);
+//     refs.overlay.addEventListener('click', overlayClickHandler);
+//     window.removeEventListener('keydown', escKeyPressHandler);
+//     window.removeEventListener('keydown', imagesScrollHandler);
+//     refs.lightbox.classList.remove('is-open');
+//     refs.image.src = '';
+//     refs.image.alt = '';
+// }
+
+// function overlayClickHandler(event) {
+//     if (event.currentTarget === event.target) {
+//         modalWindowCloseHandler();
+//     }
+// }
+
+// function escKeyPressHandler(event) {
+//     if (event.code === 'Escape') {
+//         modalWindowCloseHandler();
+//     }
+// }
+
